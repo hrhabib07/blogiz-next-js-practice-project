873dac674614d7ef719d3f3ea5ac59a05158ec38
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-09-07 12:06:03 +0600

    some new fetaures added

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,5 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { Blog } from "@/types/inex";
 
 interface BlogId {
   params: {
@@ -6,9 +7,17 @@ interface BlogId {
   };
 }
 
-const page = async ({ params }: BlogId) => {
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({ blogId: blog.id }));
+};
+
+const BlogDetailsPage = async ({ params }: BlogId) => {
   const blogId = params.blogId;
-  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`, {
+    cache: "no-store",
+  });
   const blog = await res.json();
   //   console.log(blogId);
   return (
@@ -18,4 +27,4 @@ const page = async ({ params }: BlogId) => {
   );
 };
 
-export default page;
+export default BlogDetailsPage;
