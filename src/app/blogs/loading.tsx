ba964c80e5fbaa86775c4f76835aa78ec2bcef15
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-09-10 18:49:24 +0600

    loading page added

---
+++ src/app/blogs/loading.tsx
@@ -0,0 +1,16 @@
+import LoadingCard from "@/components/ui/LoadingCard";
+import { Blog } from "@/types/inex";
+
+const BlogLoadingPage = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      {blogs.map((blog: Blog) => (
+        <LoadingCard key={blog.id}></LoadingCard>
+      ))}
+    </div>
+  );
+};
+
+export default BlogLoadingPage;
